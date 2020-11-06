219b0adf1bdcfc54ac6e02e4a05aab9e307ef3fe
siddhesh-06 <61205098+siddhesh-06@users.noreply.github.com>
2020-11-06 17:31:17 +0530

    home page

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(express.json({extend: false}));
 
 const PORT=process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send(`API running`);
-});
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('/', (req, res) => {
+    res.render('index');
+});
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/posts',require('./routes/api/posts'));
 app.use('/api/auth',require('./routes/api/auth'));
