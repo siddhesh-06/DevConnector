d762b064bc09cc5ad98d7372d0b8f96ccf925bbd
siddhesh-06 <61205098+siddhesh-06@users.noreply.github.com>
2020-11-29 22:50:01 +0530

    Errors fixed

--- server.js
+++ server.js
@@ -13,14 +13,28 @@ app.use(express.json({extend: false}));
 const PORT=process.env.PORT || 5000;
 
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// app.use(express.static(path.join(__dirname, '/client/build')));
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/posts',require('./routes/api/posts'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 
 
+//Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolved(__dirname, 'client', 'build', 'index.html')
+      )
+    );
+  }
+
+  
 app.listen(PORT,()=>console.log(`Server started on ${PORT}`));
