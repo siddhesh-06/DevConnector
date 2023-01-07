6e1d99df758fdb217afc3bfe0875d6d660a1ea40
siddheshdhainje2001 <siddheshdhainje2001@gmail.com>
2023-01-07 22:00:37 +0530

    build changes

---
+++ client/build/precache-manifest.7f3f1cd293f98eb401f5130483be71f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19f8e3065748374e09347c8cd60a5f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f217f8565faa25dd321",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "9074023609f2a2a7cfa6",
+    "url": "/static/js/2.8036014a.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.8036014a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f217f8565faa25dd321",
+    "url": "/static/js/main.7738c0c9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
